dc615f9a1c22be785c7605d5f04b91d1b7cdbc28
Paltrow <alexandriys2@ukr.net>
2023-01-11 10:59:11 +0200

    initial project setting & configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Working !!
+      <Routes>
+        {/*<Route index element={<Home />} />*/}
+        {/*<Route path="news/:id" element={<NewsPage />} />*/}
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
